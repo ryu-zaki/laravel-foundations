68bb403ddfbaa75503ac02892c33796edd776801
ryu-zaki <jhonwellryuzaki0987@gmail.com>
2026-05-16 14:54:42 +0800

    route: actual route for my server

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
+
+Route::get('/api/hello', function () {
+    return response()->json([
+        'message' => 'Hello from the Laravel backend!',
+    ]);
+});
