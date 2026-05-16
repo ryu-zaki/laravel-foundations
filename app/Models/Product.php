57306a40bb27965ff156900f762fd99f5998b5ba
ryu-zaki <jhonwellryuzaki0987@gmail.com>
2026-05-16 22:52:54 +0800

    models: User and Products Schema has been created.

---
+++ app/Models/Product.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'price',
+        'category',
+        'image',
+        'rating',
+        'stock',
+        'description',
+        'is_active',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'price' => 'float',
+        'rating' => 'float',
+        'stock' => 'integer',
+        'is_active' => 'boolean',
+    ];
+}
