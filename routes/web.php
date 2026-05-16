551ecd2b5fbf5dbe384c43dd95fdb4ab426630c7
ryu-zaki <jhonwellryuzaki0987@gmail.com>
2026-05-16 22:53:55 +0800

    routes: Endpoint of auth module for login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
@@ -8,4 +8,10 @@ Route::get('/api/hello', function () {
     return response()->json([
         'message' => 'Hello from the Laravel backend!',
     ]);
+});
+
+// Double check this isn't accidentally nested inside an auth middleware group!
+Route::middleware('guest')->group(function () {
+    Route::get('login', [LoginController::class, 'create'])->name('login');
+    Route::post('login', [LoginController::class, 'store']);
 });
